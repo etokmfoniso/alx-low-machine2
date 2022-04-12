44264f3b1c75adb8198bfeb01c2f824b63bcbe8b
Timothy Atobatele <gbeminija.oladimeji@gmail.com>
2022-04-12 22:57:47 +0100

    Task 12 commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,42 +11,36 @@ include "main.h"
 
 void print_times_table(int n)
 {
-	int row, column, product;
+	int num, mult, prod;
 
-	if (n >= 0 && n < 15)
+	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (num = 0; num <= n; num++)
 		{
-			for (column = 0; column <= n; column++)
-			{
-				product = row * column;
+			_putchar('0');
 
-				if (column == 0)
-					_putchar('0');
-				else if (product < 10)
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product % 10 + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (product >= 10 && product < 100)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar((prod / 10) + '0');
 				}
-				else if (product > 99 && product < 1000)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
 		}
