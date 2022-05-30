1da80055a0bf8254b422c87427256c0743d4bfcc
Timothy Atobatele <gbeminija.oladimeji@gmail.com>
2022-05-30 18:47:53 +0100

    Task 2 of 0x014. Get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,12 +8,14 @@
  * Return: If an error occurs - -1.
  *         Otherwise - 1.
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 8))
-		return (-1);
+    int bit_val;
 
-	*n ^= (1 << index);
+    if (index > 63)
+        return (-1);
 
-	return (1);
+    bit_val = (n >> index) & 1;
+
+    return (bit_val);
 }
